39724c16ab694893c4f6b0d9601356852e8037e2
desenvolvimento.motoca <hqt2300@gmail.com>
2021-10-27 15:05:33 -0300

    Abstração da classe Routes

--- App/Route.php
+++ App/Route.php
@@ -1,26 +1,11 @@
 <?php
 
 namespace App;
+use MF\Init\Bootstrap;
 
+class Route extends Bootstrap {
 
-class Route {
-
-    private $routes;
-
-    public function __construct() {
-        $this->initRoutes();
-        $this->run($this->getUrl());
-    }
-
-    public function getRoutes() {
-        return $this->routes;
-    }
-
-    public function setRoutes(array $routes) {
-        $this->routes = $routes;
-    }
-
-    public function initRoutes() {
+    protected function initRoutes() {
         $routes['home'] = array (
             'route' => '/',
             'controller' => 'IndexController',
@@ -35,30 +20,4 @@ class Route {
         $this->setRoutes($routes);
     }
 
-    public function run($url) {
-        foreach ($this->getRoutes() as $page => $config_page) {
-            if ($url == $config_page['route']) {
-                $class = "App\\Controllers\\"  . $config_page['controller'];
-                $controller = new $class;
-                $action = $config_page['action'];
-                $controller->$action();
-
-            }
-        }
-
-
-    }
-
-    /**
-     * 
-     * método que retorna a URL acessada pelo cliente;
-     * @return array $_SERVER super global que traz todas os detalhes do servidor; 
-     * paser_url() serve para tratar aquela url e retornar o que é [path] e o que é [query]. 
-     * Ela poder possuir parâmetros como PHP_URL_PATH que só retorna o path;
-     * 
-     */
-    
-    public function getUrl() {
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); 
-    }
 }
---
+++ vendor/MF/Init/Bootstrap.php
@@ -0,0 +1,51 @@
+<?php 
+
+namespace MF\Init;
+
+//Classe abstract tem como particularidade o fato de que ela não pode ser instanciada, somente herdada.
+abstract class Bootstrap {
+
+    private $routes;
+    //atributos/métodos abstract são atr/met que devem ser declarados na classe filha.
+    abstract protected function initRoutes();
+
+    public function __construct() {
+        $this->initRoutes();
+        $this->run($this->getUrl());
+    }
+
+    public function getRoutes() {
+        return $this->routes;
+    }
+
+    public function setRoutes(array $routes) {
+        $this->routes = $routes;
+    }
+   
+    
+    protected function run($url) {
+        foreach ($this->getRoutes() as $page => $config_page) {
+            if ($url == $config_page['route']) {
+                $class = "App\\Controllers\\"  . $config_page['controller'];
+                $controller = new $class;
+                $action = $config_page['action'];
+                $controller->$action();
+
+            }
+        }
+    }
+
+    /**
+     * 
+     * método que retorna a URL acessada pelo cliente;
+     * @return array $_SERVER super global que traz todas os detalhes do servidor; 
+     * paser_url() serve para tratar aquela url e retornar o que é [path] e o que é [query]. 
+     * Ela poder possuir parâmetros como PHP_URL_PATH que só retorna o path;
+     * 
+     */
+
+    protected function getUrl() {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); 
+    }
+}
+
