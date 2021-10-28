fe68818acd1f85c57a687296e963a42062d6b175
desenvolvimento.motoca <hqt2300@gmail.com>
2021-10-28 15:36:32 -0300

    Adicionando o método render no IndexController

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -4,12 +4,32 @@ namespace App\Controllers;
 
 class IndexController {
 
+    private $view;
+
+    public function __construct() {
+        $this->view = new \stdClass(); /*A StdClass é uma classe predefinida do PHP. Ela é vazia, ou seja, 
+        não possui métodos nem propriedades. É útil também utilizar a StdClass quando
+        se deseja criar um objeto vazio e ir adicionando as propriedades conforme necessário.*/
+    }
+
     public function index() {
-       require_once "../App/Views/index/index.phtml";
+        $this->view->dados = array('item1', 'item2', 'item3');
+        $this->render('index');
     }
 
     public function sobreNos() {
-        require_once "../App/Views/index/sobreNos.phtml";
+        $this->view->dados = array('item4', 'item5'); 
+        $this->render('sobreNos');
+    }
+
+    public function render($view) {
+        
+        $class = get_class($this);
+        $class = str_replace('App\\Controllers\\', '', $class);
+        $class = strtolower(str_replace('Controller', '', $class));
+
+        require_once "../App/Views/".$class."/".$view.".phtml";
+
     }
 }
 
--- public/index.php
+++ public/index.php
@@ -3,3 +3,4 @@
 require_once "../vendor/autoload.php";
 
 $route = new \App\Route;
+        
